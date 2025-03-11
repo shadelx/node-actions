0e51a6986bb8cd0e2fc1a2d53fc3461cec1a905c
shadelx <shadelx.07@gmail.com>
2025-03-11 10:23:07 -0600

    feat: add different environments

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+// Load environment variables based on NODE_ENV
+const env = process.env.NODE_ENV || "development";
+dotenv.config({ path: `.env.${env}` });
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(process.env.MESSAGE || "Hello World");
 });
 
 module.exports = app;
